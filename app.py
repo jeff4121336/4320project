732dbeb230b2bc9a5aa7bf199df12dce3d58d26f
jeff4121336 <jeffyau36@gmail.com>
2023-12-22 18:35:45 +0800

    upload file function success

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template, request
+from pydub import AudioSegment
 import os
+import speech_recognition as sr
+from flask import Flask, flash, request, redirect, render_template, url_for
+from werkzeug.utils import secure_filename
+
 
 app = Flask(__name__)
 
@@ -7,26 +11,66 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
-@app.route('/audio')
-def check():
-    dir = os.listdir('./audio')
-    if len(dir) != 0:
-        return '1'
-    else:
-        return '0'
+ALLOWED_EXTENSIONS = {'wav'}
+path = os.getcwd()
+UPLOAD_FOLDER = os.path.join(path, 'uploads')
 
-# route for getting the user uploaded files
-@app.route('/upload', methods=['POST'])
-def upload():
-    fileitem = request.files['file']
+if not os.path.isdir(UPLOAD_FOLDER):
+    os.mkdir(UPLOAD_FOLDER)
 
-    if fileitem:
-        upload_dir = './audio'
-        file_path = os.path.join(upload_dir, fileitem.filename)
-        fileitem.save(file_path)
-        return render_template("home.html")
-    
-    return 'File uploaded errors'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def removePreviousFiles():
+    for filename in os.listdir(UPLOAD_FOLDER):
+        file_path = os.path.join(UPLOAD_FOLDER, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+        except Exception as e:
+            print(f'Failed to delete {file_path}. Reason: {e}')
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['POST'])
+def upload_file():
+    removePreviousFiles()
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            audioToText(filename)
+            return redirect(request.url)
+    return ''
+
+def audioToText(filename):
+    recognizer = sr.Recognizer()
+
+    audio = UPLOAD_FOLDER + '/' + filename
+
+    with sr.AudioFile(audio) as source:
+        audio_data = recognizer.record(source)
+        try:
+            text = recognizer.recognize_google(audio_data, language="en-US")
+            print(f"You said: {text}")
+            return render_template("home.html", text=text, originalAudio=audio)
+        except sr.UnknownValueError:
+            print("Sorry, I couldn't understand that.")
+            return render_template("home.html", text="Sorry, I couldn't understand that.", originalAudio=audio)
+        except sr.RequestError:
+            print("Sorry, there was an error processing your request.")
+            return render_template("home.html", text="Sorry, there was an error processing your request.", originalAudio=audio)
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=3000, debug=True)
